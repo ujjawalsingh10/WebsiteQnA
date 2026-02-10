04bcccd3aeecf406982be6d387220e76b959b937
ujjawalsingh10 <iamujjawal10.us@gmail.com>
2026-02-10 15:12:33 +0530

    LLM completed

---
+++ app/components/llm.py
@@ -0,0 +1,32 @@
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_huggingface import ChatHuggingFace
+
+from app.common.logger import get_logger
+from app.common.custom_exceptions import CustomException
+from app.config.config import HUGGINGFACE_REPO_ID, HF_TOKEN
+
+logger = get_logger(__name__)
+
+def load_llm(huggingface_repo_id: str = HUGGINGFACE_REPO_ID, hf_token: str = HF_TOKEN):
+    try:
+        logger.info('Loading LLM from Huggingface')
+        llm_endpoint = HuggingFaceEndpoint(
+            repo_id=HUGGINGFACE_REPO_ID,
+            task='text-generation',
+            huggingfacehub_api_token=HF_TOKEN,
+            max_new_tokens=256,
+            temperature=0.2
+        )
+
+        llm = ChatHuggingFace(llm=llm_endpoint)
+        logger.info('LLM loaded successfully...')
+        return llm
+    
+    except Exception as e:
+        custom_error = CustomException(
+            message="Failed to load a LLM",
+            error_detail=e
+        )
+        logger.exception(custom_error)
+        raise custom_error
+
--- app/config/config.py
+++ app/config/config.py
@@ -1,6 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 CHUNK_SIZE = 600
 CHUNK_OVERLAP = 50
 
+HF_TOKEN = os.getenv('HF_TOKEN')
+HUGGINGFACE_REPO_ID = 'meta-llama/Llama-3.1-8B-Instruct'
 HUGGINGFACE_EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 DB_FAISS_PATH = 'vectorstore/db_faiss'
 # DATA_PATH ='data/'
